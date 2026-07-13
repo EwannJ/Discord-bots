55957205ca0412e377395b89034b63fc4dc3736d
Ewann JACQUELIN <ew.jacquelin@gmail.com>
2026-07-13 19:08:31 +0200

    SKR

--- SKR/skr.py
+++ SKR/skr.py
@@ -5,15 +5,15 @@ from discord.ext import commands
 from discord import app_commands
 
 load_dotenv()
-toekn = os.getenv('TOKEN')
+TOKEN = os.getenv('TOKEN')
 
 intents = discord.Intents.all()
-bot = SKRBot(command_prefix=commands.when_mentioned, intents=intents, help_command=None)
+bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents, help_command=None)
 
-@app_commands.command(name="ping", description="Voir le ping du bot")
+@bot.tree.command(name="ping", description="Voir le ping du bot")
 @app_commands.checks.has_permissions(manage_messages=True)
-async def ping(self, interaction: discord.Interaction):
-    latency = round(self.bot.latency * 1000)
+async def ping(interaction: discord.Interaction):
+    latency = round(bot.latency * 1000)
     await interaction.response.send_message(f"🏓 Pong ! Ma latence est de **{latency}ms**.", ephemeral=True)
 
 @bot.event
@@ -23,6 +23,6 @@ async def on_ready():
         synced = await bot.tree.sync()
         print(f"{len(synced)} commandes syncronisées")
     except Exception as e:
-        print(f"/!\ Erreur lors de la synchronisation des commandes : {e}")
+        print(f"⚠️ Erreur lors de la synchronisation des commandes : {e}")
 
 bot.run(TOKEN)
